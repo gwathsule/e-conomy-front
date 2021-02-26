e2773dadc60dabf5706856092449e8cc3c51fbae
Rafael dos Santos Gomes <santos@imaginationmedia.com>
2021-02-26 18:03:25 -0300

    grid layout da page principal

--- src/pages/Jogo/index.js
+++ src/pages/Jogo/index.js
@@ -1,16 +1,17 @@
 import React, {useContext, useEffect} from 'react';
 import {useSelector, useDispatch} from "react-redux";
-import { context } from '../../services/AuthContext'
-import { jogoRequest } from '../../store/modules/jogo/actions'
+import { context } from '../../services/AuthContext';
+import { jogoRequest } from '../../store/modules/jogo/actions';
 import {clearToken, getToken} from "../../helpers/storeToken";
-import Notification from '../../helpers/notification'
+import Notification from '../../helpers/notification';
+import {JogoBoxWrapper} from "./style";
+import {Col, Container, Row} from "reactstrap";
 
 const Jogo = () => {
     const { handleLogout } = useContext(context);
     const dispatch = useDispatch();
     const token = getToken();
     var jogoState = useSelector(state => state.jogo);
-    console.log(jogoState);
     useEffect(() => {
         if(jogoState.jogo === null) {
             try {
@@ -28,10 +29,27 @@ const Jogo = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Jogo</h1>
-            <button type={"button"} onClick={handleLogout}>Sair</button>
-        </div>
+        <JogoBoxWrapper>
+            <Container fluid={true} className={"boxJogo"}>
+                <Row >
+                    <Col xs={12} className={"boxGrafico"}>
+                        boxGrafico
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={3}>
+                        <Row><div className={"boxPerfil"}>boxPerfil</div></Row>
+                        <Row><div className={"boxUltimoMes"}>boxUltimoMes</div></Row>
+                    </Col>
+                    <Col xs={6}><div className={"boxTimeline"}>boxTimeline</div></Col>
+                    <Col xs={3}>
+                        <Row><div className={"boxPopularidade"}>boxPopularidade</div></Row>
+                        <Row><div className={"boxMedidas"}>boxMedidas</div></Row>
+                    </Col>
+                </Row>
+            </Container>
+        </JogoBoxWrapper>
     );
 }
 
--- src/pages/Jogo/style.js
+++ src/pages/Jogo/style.js
@@ -1,9 +1,50 @@
 import styled from 'styled-components/macro';
 
 export const JogoBoxWrapper = styled.div`
-    margin-top: 10%;
     display: grid;
     justify-content: center;
+
+    .boxJogo{
+      margin-top: 5%;
+      width: 1020px;
+      background-color: rgba(255, 255, 255, 0.5);
+      padding: 15px 30px;
+      border-radius: 4px;
+    }
     
+    .boxGrafico{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+      margin-bottom: 15px;
+    }
     
+    .boxPerfil{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+      margin-bottom: 15px;
+    }
+    .boxUltimoMes{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+    }
+    
+    .boxTimeline{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+    }
+    .boxPopularidade{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+      margin-bottom: 15px;
+    }
+    .boxMedidas{
+      width: 100%;
+      height: 150px;
+      background-color: #FFF;
+    }
 `;
